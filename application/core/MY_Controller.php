0c461469cc7160bb08a56d2ca8caa80158073062
Tanmoy Das <tanmoy@grmtech.com>
2016-09-06 02:15:27 -0700

    In the config array if alert-email exists then it will send out an email

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -6,7 +6,9 @@ class MY_Controller extends CI_Controller{
 		if(!$servers[$server]) die("Invalid server: ".$server);
 		
 		$config = $servers[$server];
-		
+
+		//print_r($config);
+
 		$this->load->library('xmlrpc',array(),$server);
 		$this->{$server}->initialize();
 		$this->{$server}->server($config['url'],$config['port']);
@@ -23,7 +25,26 @@ class MY_Controller extends CI_Controller{
 		}else{
 				$response = $this->{$server}->display_response();
 		}	
+		/*
+		The response is an array containing all the services and thier state from the supervisor for 1 superversior deamon
+		so if there are 10 supervisor daemons this will be called 10 times.
+		echo "printing response";
 		
+		*/
+		//print_r($response);
+		if( $config['alert-email']!=NULL){
+		    for($i=0;$i<sizeof($response);$i++){
+                      $currentState = trim($response[$i]['statename']);
+		      //echo "The current state is $currentState <br>";		    
+		      $comparisonValue = substr_compare($currentState,"RUNNING",0,8);
+		      if($comparisonValue != 0){
+		        echo $comparisonValue;			  
+		        echo "sending alert email";
+			echo "Subject: Service " . $response[$i]['name'] . " on server " . $config['url'] . " is down";
+		      }
+		    }
+		
+		}
 		return $response;
 	}
 
